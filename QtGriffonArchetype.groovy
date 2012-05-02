7592c9f8b1f5529398d3774a120f000c109bc61e
aalmiray <aalmiray@gmail.com>
2012-05-02 16:32:09 +0200

    First commit

---
+++ QtGriffonArchetype.groovy
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+class QtGriffonArchetype {
+    String version = '1.0'
+    String griffonVersion = '0.9.6 > *'
+    String license = 'Apache Software License 2.0'
+    String documentation = ''
+    String source = 'https://github.com/griffon/griffon-qt-archetype'
+    List authors = [
+        [
+            name: 'Andres Almiray',
+            email: 'aalmiray@yahoo.com'
+        ]
+    ]
+    String title = 'Default archetype for Qt applications'
+    String description = '''
+Brief description of Qt.
+
+Usage
+----
+Simply specify the name of the archetype (*qt*) when invoking the `create-app`
+command, like this
+
+    griffon create-app sample -archetype=qt
+
+Configuration
+-------------
+There's no additional configuration required by this archetype.'
+'''
+}
